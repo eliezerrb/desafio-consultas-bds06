6200b59bcfbdbbc294225d904524102a9e86d4c5
eliezerrb <eliezerrezende@outlook.com>
2022-11-10 21:45:59 -0300

    first commit - implement movieById, MovieReviews, MovieByGenre, All genres

---
+++ src/main/java/com/devsuperior/movieflix/services/ReviewService.java
@@ -0,0 +1,45 @@
+package com.devsuperior.movieflix.services;
+
+import java.util.List;
+import java.util.stream.Collectors;
+
+import javax.persistence.EntityNotFoundException;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.stereotype.Service;
+import org.springframework.transaction.annotation.Transactional;
+
+import com.devsuperior.movieflix.dto.ReviewDTO;
+import com.devsuperior.movieflix.entities.Movie;
+import com.devsuperior.movieflix.entities.Review;
+import com.devsuperior.movieflix.repositories.MovieRepository;
+import com.devsuperior.movieflix.repositories.ReviewRepository;
+import com.devsuperior.movieflix.services.exceptions.ResourceNotFoundException;
+
+@Service
+public class ReviewService {
+	
+	@Autowired
+	private ReviewRepository repository;
+	
+	@Autowired
+	private MovieRepository movieRepository;
+	
+	@Autowired
+	private AuthService authService;
+	
+	@Transactional(readOnly = true)
+	public List<ReviewDTO> findByMovie(Long movieId){
+		try {
+			Movie movie = movieRepository.getOne(movieId);
+			List<Review> list = repository.findByMovie(movie);
+			return list.stream().map(x -> new ReviewDTO(x)).collect(Collectors.toList());
+		} 
+		catch (EntityNotFoundException e) {
+			throw new ResourceNotFoundException("Id not found" + movieId);
+		}
+	}
+	
+	
+
+}
